e56838943448d9f5ab07e3f002c0a76549fdec25
Tagor Murilo <tagor808@gmail.com>
2026-04-01 15:16:29 -0300

    Inicio do projeto, estrutura inicial de pastas

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './common/styles/global.css'
+
+function App() {
+  return (
+    <div>
+      <h1>🏛️ Sistema Iniciado</h1>
+    </div>
+  );
+}
+
+export default App;
