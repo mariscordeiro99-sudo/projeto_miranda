11a334826acfa2aef2d6328040390e8ef7277e2f
Pablo <pabloleonardo93@gmail.com>
2026-05-21 11:22:10 -0300

    perf: aplicar compressao de arquivos estaticos com whitenoise

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -233,6 +233,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
@@ -249,3 +251,13 @@ DATABASES = {
         },
     }
 }
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
